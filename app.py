212c3e646588897745b9379e444675d864130a50
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2024-07-07 12:40:40 -0300

    Adição da rota raiz (página principal) e função que será executada.

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ from flask import Flask
 # Criação de uma variável app que receberá uma instância de Flask
 # default configuration
 app = Flask(__name__)
+
+# Definindo uma rota raiz (página inicial)  a função que será executada ao requisitar
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+if __name__ == "__main__":
+    app.run(debug=True)
